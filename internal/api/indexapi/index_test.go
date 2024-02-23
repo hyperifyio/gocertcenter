d992951289c4d859faf62d13a76e0365530a6db6
Jaakko Heusala <jhh@hg.fi>
2024-02-23 18:54:48 +0200

    Fixed tests for https://github.com/hyperifyio/gocertcenter/issues/105

--- internal/api/indexapi/index_test.go
+++ internal/api/indexapi/index_test.go
@@ -10,37 +10,16 @@ import (
 )
 
 func TestIndex(t *testing.T) {
+	mockResponse := &mocks.MockResponse{}
+	mockRequest := &mocks.MockRequest{IsGet: true}
+	mockServer := mocks.NewMockServer()
 
-	tests := []struct {
-		name          string
-		requestMethod bool // true for GET, false otherwise
-		expectError   bool
-	}{
-		{"GET Request", true, false},
-		{"Non-GET Request", false, true},
+	indexapi.Index(mockResponse, mockRequest, mockServer)
+
+	if mockResponse.SentStatusCode != 200 {
+		t.Errorf("Expected status code 200, got %d", mockResponse.SentStatusCode)
 	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			mockResponse := &mocks.MockResponse{}
-			mockRequest := &mocks.MockRequest{IsGet: tt.requestMethod}
-			mockServer := mocks.NewMockServer()
-
-			indexapi.Index(mockResponse, mockRequest, mockServer)
-
-			if tt.expectError {
-				if !mockResponse.MethodNotSupportedError {
-					t.Errorf("Expected method not allowed error, but didn't get one")
-				}
-			} else {
-				if mockResponse.SentStatusCode != 200 {
-					t.Errorf("Expected status code 200, got %d", mockResponse.SentStatusCode)
-				}
-				if data, ok := mockResponse.SentData.(dtos.IndexDTO); !ok || data.Version != "0.0.1" {
-					t.Errorf("Expected data version 0.0.1, got %v", mockResponse.SentData)
-				}
-			}
-		})
+	if data, ok := mockResponse.SentData.(dtos.IndexDTO); !ok || data.Version != "0.0.1" {
+		t.Errorf("Expected data version 0.0.1, got %v", mockResponse.SentData)
 	}
-
 }
--- internal/dtos/index_test.go
+++ internal/dtos/index_test.go
@@ -9,19 +9,20 @@ import (
 
 func TestNewIndexDTO(t *testing.T) {
 	tests := []struct {
+		name    string
 		version string
 		want    dtos.IndexDTO
 	}{
-		{"1.0.0", dtos.IndexDTO{Version: "1.0.0"}},
-		{"2.0.1", dtos.IndexDTO{Version: "2.0.1"}},
+		{"gocertcenter", "1.0.0", dtos.IndexDTO{Name: "gocertcenter", Version: "1.0.0"}},
+		{"gocertcenter", "2.0.1", dtos.IndexDTO{Name: "gocertcenter", Version: "2.0.1"}},
 		// Add more test cases as needed
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.version, func(t *testing.T) {
-			got := dtos.NewIndexDTO(tt.version)
+			got := dtos.NewIndexDTO(tt.name, tt.version)
 			if got.Version != tt.want.Version {
-				t.Errorf("NewIndexDTO(%s) = %v, want %v", tt.version, got, tt.want)
+				t.Errorf("NewIndexDTO(%s, %s) = %v, want %v", tt.name, tt.version, got, tt.want)
 			}
 		})
 	}
